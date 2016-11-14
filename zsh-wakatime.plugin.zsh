4211a58fa960ca5dc5bc2501c58c2c24203a709c
Yuriy Grechanyi <ygrechanyi@lohika.com>
2016-11-14 16:23:17 +0200

    Added project detection for python
    Relative to virtualenv/virtualenvwrapped project variables:
    ${VIRTUAL_ENV}
    ${VIRTUALENVWRAPPER_PROJECT_FILENAME}
    
    See more: https://virtualenvwrapper.readthedocs.io/en/latest/projects.html#project-management

--- zsh-wakatime.plugin.zsh
+++ zsh-wakatime.plugin.zsh
@@ -3,8 +3,12 @@
 # hook function to send wakatime a tick
 send_wakatime_heartbeat() {
     entity=$(waka_filename);
+    project=$(waka_project);
     if [ "$entity" ]; then
-        (wakatime --write --plugin "zsh-wakatime/0.0.1" --entity-type app --project Terminal --entity "$entity"> /dev/null 2>&1 &)
+        if [ -z "$project" ]; then
+            project="Terminal"
+        fi
+        (wakatime --write --plugin "zsh-wakatime/0.0.1" --entity-type app --project "$project" --entity "$entity"> /dev/null 2>&1 &)
     fi
 }
 
@@ -16,6 +20,12 @@ waka_filename() {
     else
         # only command without arguments to avoid senstive information
         echo "$history[$((HISTCMD-1))]" | cut -d ' ' -f1
+    fi
+}
+
+waka_project() {
+    if [ ! -z $VIRTUAL_ENV ] && [ ! -z $VIRTUALENVWRAPPER_PROJECT_FILENAME ]; then
+        basename $(cat $VIRTUAL_ENV/$VIRTUALENVWRAPPER_PROJECT_FILENAME)
     fi
 }
 
